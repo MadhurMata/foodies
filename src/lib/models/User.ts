b0971415d2856268e8eb26bc08fe55e25c0a8590
MadhurMataG <madhur.mata@appspace.com>
2024-05-15 09:09:24 +0200

    user model

--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -1,15 +1,34 @@
 import mongoose, { Document, Schema } from 'mongoose';
-import { IRestaurant } from './Restaurants';
 
 export interface IUser extends Document {
   name: string;
+  surname: string;
   nickName: string;
   email: string;
-  resraurantsDiscovered: IRestaurant[];
+  password: string;
+  phoneNumber?: string;
+  thumnail?: string;
+  bio?: string;
+  savedLocation?: string;
+  favouriteRestaurants?: Schema.Types.ObjectId[];
+  wantToGoRestaurants?: Schema.Types.ObjectId[];
+  discoveredRestaurants?: Schema.Types.ObjectId[];
+  following?: Schema.Types.ObjectId[];
+  Followers?: Schema.Types.ObjectId[];
+  privacy: {
+    openProfile: boolean;
+  };
+  ratings?: Schema.Types.ObjectId[];
+  averageRating?: number;
+  userLevel?: string;
 }
 
 const userSchema = new mongoose.Schema({
   name: {
+    type: String,
+    requied: true,
+  },
+  surname: {
     type: String,
     requied: true,
   },
@@ -21,12 +40,27 @@ const userSchema = new mongoose.Schema({
     type: String,
     requied: true,
   },
-  resraurantsDiscovered: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Restaurant',
-    },
-  ],
+  password: {
+    type: String,
+    requied: true,
+  },
+  phoneNumber: String,
+  thumnail: String,
+  bio: String,
+  savedLocation: String,
+  favouriteRestaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant' }],
+  wantToGoRestaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant' }],
+  discoveredRestaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant' }],
+  following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  Followers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  privacy: {
+    openProfile: Boolean,
+    requied: true,
+    default: false,
+  },
+  ratings: [{ type: Schema.Types.ObjectId, ref: 'Rating' }],
+  averageRating: Number,
+  userLevel: String,
 });
 
 const User = mongoose.model<IUser>('User', userSchema);
