dbf3c8dbf8685d9fa4a5ba96d2a04d1cfa3e66dd
MadhurMataG <madhur.mata@appspace.com>
2024-05-28 13:59:10 +0200

    eslint warnings fix

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -3,23 +3,23 @@ type ArrowKeyValues = 'up' | 'down' | 'left' | 'right';
 type KeyValues = 'escape' | 'enter' | 'numpadEnter' | 'space' | 'backspace';
 
 type ArrowKey = {
-	[key in ArrowKeyValues]: `Arrow${Capitalize<key>}`;
+  [key in ArrowKeyValues]: `Arrow${Capitalize<key>}`;
 };
 
 type Key = {
-	[key in KeyValues]: Capitalize<key>;
+  [key in KeyValues]: Capitalize<key>;
 };
 
 const KEYS: ArrowKey & Key = {
-	up: 'ArrowUp',
-	down: 'ArrowDown',
-	left: 'ArrowLeft',
-	right: 'ArrowRight',
-	escape: 'Escape',
-	enter: 'Enter',
-	numpadEnter: 'NumpadEnter',
-	space: 'Space',
-	backspace: 'Backspace'
+  up: 'ArrowUp',
+  down: 'ArrowDown',
+  left: 'ArrowLeft',
+  right: 'ArrowRight',
+  escape: 'Escape',
+  enter: 'Enter',
+  numpadEnter: 'NumpadEnter',
+  space: 'Space',
+  backspace: 'Backspace',
 };
 
 export default KEYS;
