b95cf52c56dc9d7b73ff5eb6f308cdef1174ddb1
MadhurMataG <madhur.mata@appspace.com>
2024-05-30 18:37:33 +0200

    header profile

--- src/app/[userName]/page.tsx
+++ src/app/[userName]/page.tsx
@@ -7,7 +7,26 @@ const UserProfile = ({ params }: { params: { userName: string } }) => {
     userName: params.userName,
   });
 
-  return <div>{user && user[0].firstName}</div>;
+  return (
+    <div className="flex w-full gap-6 p-6">
+      <div>
+        <img
+          className="h-20 w-20 rounded-full shadow-lg"
+          alt="Imagen de perfil"
+          src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+        />
+      </div>
+      <div>
+        <h2 className="text-lg font-bold">{user && user[0].firstName}</h2>
+        <button
+          type="button"
+          className="shadow-light-3 hover:shadow-light-2 inline-block rounded-md bg-neutral-400 px-6 py-2 transition duration-150 ease-in-out hover:bg-neutral-200 focus:bg-neutral-200"
+        >
+          Editar perfil
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
